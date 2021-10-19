306296c7d31f6300cbb833d9bd7da6f8eb36f1cb
Mike <korostelevm@gmail.com>
2021-10-19 11:19:41 -0400

    asdf

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
     console.log("A GET request!")
+    console.log(req.headers)
     res.json({Hello:'asdfasdfsdf'})
 })
 app.listen(process.env.PORT || 3000)
